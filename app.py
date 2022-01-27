03b0a20123ffd2ecc6ac0449c79b09d7f0c214e1
Syndragami <adcarrymystic@gmail.com>
2022-01-27 22:23:42 +0100

    Made graphs run on web server

--- app.py
+++ app.py
@@ -2,58 +2,102 @@ from jbi100_app.main import app
 from jbi100_app.views.menu import make_menu_layout
 from jbi100_app.views.scatterplot import Scatterplot
 from jbi100_app.data import get_data
-
-from dash import html
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
 import plotly.express as px
+import plotly.graph_objects as go
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output
 
 
 if __name__ == '__main__':
     # Create data
-    get_data()
+    df = get_data()
 
     # Instantiate custom views
-    scatterplot1 = Scatterplot("Scatterplot 1", 'sepal_length', 'sepal_width', df)
-    scatterplot2 = Scatterplot("Scatterplot 2", 'petal_length', 'petal_width', df)
+    scatterplot1 = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", color="Accident_Severity",
+                  #color_continuous_scale=px.colors.cyclical.IceFire, 
+                  size_max=3, zoom=5,
+                  hover_data=['Date','Time'],
+                  mapbox_style="carto-positron",
+                  animation_frame="Month",
+                  title='Traffic accidents in the UK in 2015')
+    scatterplot1.update_traces(marker_size=3, opacity=0.5)                  
 
-    app.layout = html.Div(
-        id="app-container",
-        children=[
-            # Left column
-            html.Div(
-                id="left-column",
-                className="three columns",
-                children=make_menu_layout()
-            ),
+    scatterplot2 = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", color="Sex_of_Driver",
+                  #color_continuous_scale=px.colors.cyclical.IceFire, 
+                  size='Accident_Severity',
+                  size_max=8, zoom=5,
+                  hover_data=['Date','Time'],
+                  mapbox_style="carto-positron",
+                  animation_frame="Month",
+                  title='Traffic accidents in the UK in 2015')
+    scatterplot2.update_traces(marker_size=3, opacity=0.5)                  
 
-            # Right column
-            html.Div(
-                id="right-column",
-                className="nine columns",
-                children=[
-                    scatterplot1,
-                    scatterplot2
-                ],
+    heatmap = px.density_mapbox(df, lat='Latitude', lon='Longitude', z="Accident_Severity", radius=3,
+                        #center=dict(lat=0, lon=180), 
+                        zoom=5,
+                        mapbox_style="stamen-terrain",
+                        animation_frame="Month",
+                        title='Traffic accidents in the UK in 2015')
+
+    app.layout = html.Div(children=[
+        # All elements from the top of the page
+        html.Div([
+            html.H1(children='Hello Dash'),
+
+            html.Div(children='''
+                Dash: A web application framework for Python.
+            '''),
+
+            dcc.Graph(
+                id='scatterplot1',
+                figure=scatterplot1
+            ),  
+        ]),
+        # New Div for all elements in the new 'row' of the page
+        html.Div([ 
+            dcc.Graph(
+                id='scatterplot2',
+                figure = scatterplot2
             ),
-        ],
+            html.Label([
+                "colorscale",
+                dcc.Dropdown(
+                    id='colorscale-dropdown', clearable=False,
+                    value='bluyl', options=[
+                        {'label': c, 'value': c}
+                        for c in px.colors.named_colorscales()
+                    ])
+            ]),
+        ]),
+        html.Div([ 
+            dcc.Graph(
+                id='heatmap',
+                figure = heatmap
+            ),
+        ])
+    ])
+
+    # Callback function that automatically updates the tip-graph based on chosen colorscale
+    @app.callback(
+        Output('scatterplot2', 'figure'),
+        [Input("colorscale-dropdown", "value")]
     )
-
-    # Define interactions
-    @app.callback(
-        Output(scatterplot1.html_id, "figure"), [
-        Input("select-color-scatter-1", "value"),
-        Input(scatterplot2.html_id, 'selectedData')
-    ])
-    def update_scatter_1(selected_color, selected_data):
-        return scatterplot1.update(selected_color, selected_data)
-
-    @app.callback(
-        Output(scatterplot2.html_id, "figure"), [
-        Input("select-color-scatter-2", "value"),
-        Input(scatterplot1.html_id, 'selectedData')
-    ])
-    def update_scatter_2(selected_color, selected_data):
-        return scatterplot2.update(selected_color, selected_data)
+    def update_scatterplot(colorscale):
+        return px.scatter_mapbox(
+            df, lat="Latitude", lon="Longitude", color="Accident_Severity",
+            colorscale=px.colors.cyclical.IceFire, 
+            size_max=3, zoom=5,
+            hover_data=['Date','Time'],
+            mapbox_style="carto-positron",
+            animation_frame="Month",
+            title='Traffic accidents in the UK in 2015',
+            render_mode="webgl"
+        )
 
 
     app.run_server(debug=False, dev_tools_ui=False)
--- jbi100_app/data.py
+++ jbi100_app/data.py
@@ -1,9 +1,21 @@
 import pandas as pd
+import numpy as np # linear algebra
 
 def get_data():
     # Read data
-    df = pd.read_csv('data/dataset.csv')
+    df = pd.read_csv('jbi100_app/data/dataset.csv')
 
     # Any further data preprocessing can go here
+    df = df.replace('?', np.nan)
+    roads = df.drop(columns=['Junction_Control', '2nd_Road_Class','Age_of_Vehicle','Engine_Capacity_(CC)','Propulsion_Code','Driver_Home_Area_Type'])
+    roads = roads.dropna()
+    intermediate = roads.drop(columns=['Accident_Index','Location_Easting_OSGR','Location_Northing_OSGR','Longitude','Latitude','Date','Time','Local_Authority_(Highway)','LSOA_of_Accident_Location'])
+    intermediate = intermediate.astype('int64')
+    intermediate2 = roads[['Accident_Index','Location_Easting_OSGR','Location_Northing_OSGR','Longitude','Latitude','Date','Time','Local_Authority_(Highway)','LSOA_of_Accident_Location']]
+    road_clean = intermediate2.join(intermediate)
+    road_clean['Longitude'] = road_clean['Longitude'].astype('float64')
+    road_clean['Latitude'] = road_clean['Latitude'].astype('float64')
+    road_clean['Year']=road_clean['Accident_Index'].str[:4]
+    road_clean['Month']=road_clean['Date'].str[3:5]
 
-    return df
+    return road_clean
